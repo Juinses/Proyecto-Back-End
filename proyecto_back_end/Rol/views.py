f1af502e4bccf95a4c8a76d7fb2fe857666f9d94
Milo <jmalvarez.tec@gmail.com>
2025-09-04 18:42:17 -0400

    entrega

--- proyecto_back_end/Rol/views.py
+++ proyecto_back_end/Rol/views.py
@@ -1,20 +1,19 @@
 from django.shortcuts import render
-from Rol import models as DatosRol 
+from Rol import models as DatosRol
 
 def seleccion_rol(request):
     # Pasamos todo el diccionario de tipos de rol al template
     data = {
-        "juegos_rol": DatosRol.juegos_rol
+        "juegos_rol": DatosRol.juegos_rol,
     }
     return render(request, 'templatesrol/seleccion_rol.html', data)
 
 def Tipos_rol(request, tipo):
     # Obtenemos la lista de juegos para el tipo seleccionado
     juegos = DatosRol.juegos_rol.get(tipo, [])
-    
     context = {
         "tipo": tipo,
-        "juegos": juegos
+        "juegos": juegos,
     }
     return render(request, 'templatesrol/Tipos_rol.html', context)
 
