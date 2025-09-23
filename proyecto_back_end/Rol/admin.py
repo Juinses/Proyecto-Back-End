80182573f147ee8700e751f9e95477a5224d08e1
Laboratorio Computación N° 7 <claseslab7@inacap.cl>
2025-09-23 20:04:09 -0300

    Pequeños cambios

--- proyecto_back_end/Rol/admin.py
+++ proyecto_back_end/Rol/admin.py
@@ -7,15 +7,3 @@ class GeneroAdmin(admin.ModelAdmin):
     list_display = ("nombre",)
     search_fields = ("nombre",)
 
-
-@admin.register(Juego)
-class JuegoAdmin(admin.ModelAdmin):
-    list_display = ("nombre", "genero", "mostrar_imagen")
-    list_filter = ("genero",)
-    search_fields = ("nombre", "descripcion")
-
-    def mostrar_imagen(self, obj):
-        if obj.imagen:
-            return format_html('<img src="{}" width="50" height="50" />', obj.imagen.url)
-        return "Sin imagen"
-    mostrar_imagen.short_description = "Imagen"
